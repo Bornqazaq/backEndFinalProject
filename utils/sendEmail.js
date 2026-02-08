68217b7908d7003e4f48b868b6a669993ceead7f
user <user@example.com>
2026-02-09 00:43:46 +0500

    Fix: edge cases

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,19 +1,23 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
+  service: 'gmail',
   host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT || 587,
-  secure: false,
+  port: parseInt(process.env.SMTP_PORT) || 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
 module.exports = async (to, subject, text) => {
   try {
     const info = await transporter.sendMail({
-      from: "TaskManager <nehabarbro@gmail.com>",
+      from: `TaskManager <${process.env.SMTP_USER}>`,
       to,
       subject,
       text
